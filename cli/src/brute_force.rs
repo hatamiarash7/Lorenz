f5c5d2092f7e7b9f40030d364923149227b897e1
Arash Hatami <hatamiarash7@gmail.com>
2020-09-03 15:31:30 +0430

    Add cli

---
+++ cli/src/brute_force.rs
@@ -0,0 +1,61 @@
+
+#[cfg(test)]
+mod tests {
+    use rand::{thread_rng, RngCore};
+    use std::io::Write;
+    use itertools::Itertools;
+    use std::time::Instant;
+
+    #[test]
+    fn brute_force_test() -> Result<(), Box<dyn std::error::Error>> {
+
+        let mut random_data = vec![0; (1<<10) * 100]; // 100KiB
+        thread_rng().fill_bytes(&mut random_data);
+        let mut temp_file = std::env::temp_dir();
+        temp_file.push("rand.txt");
+        let mut file = std::fs::File::create(&temp_file)?;
+        file.write_all(&random_data)?;
+
+        let pw = "abcdefghijkl".to_string();
+        let in_file = temp_file.to_str().unwrap().to_string();
+        let mut out_path = std::env::temp_dir();
+        out_path.push("encrypted.txt");
+        let out_file = out_path.to_str().unwrap().to_string();
+        let config = lorenz::Config::new(&lorenz::Mode::Encrypt, pw, &in_file, &out_file);
+        lorenz::main_routine(&config)?;
+
+        let mut possible_chars = ('a'..='z').collect::<Vec<char>>();
+        possible_chars.append(&mut ('A'..='Z').collect());
+        possible_chars.append(&mut "0123456789!@#$%^&*()-_=+`~,./<>?;':\"[]{}\\|".chars().collect());
+        let mut combiner = possible_chars.iter().combinations_with_replacement(10);
+        println!("possible chars: {}", possible_chars.len());
+
+        let num_combos = 1951641934005400.;
+        let start_time = Instant::now();
+        let mut attempts = 0;
+
+        loop {
+            let guess_chars = combiner
+                .next() 
+                .ok_or("end of combinations")? 
+                .iter()
+                .cloned().cloned()
+                .collect::<Vec<char>>(); 
+            let guess: String = guess_chars.into_iter().collect();
+            let c = lorenz::Config::new(&lorenz::Mode::Decrypt, guess.clone(), &out_file, "./result");
+            assert!(lorenz::main_routine(&c).is_err());
+
+            attempts += 1;
+            let elapsed = Instant::now().duration_since(start_time.clone()).as_secs_f64();
+            if elapsed == 0. {continue};
+            let attempts_per_sec = attempts as f64 / elapsed;
+            let num_secs = num_combos / attempts_per_sec;
+            let num_years = num_secs / (60. * 60. * 24. * 365.);
+            if attempts % 100 == 0 {
+                println!("guess: {}", guess);
+                println!("at {:.3} attempts per second, it would take {:.2} years to test all 12-character passwords including lower-/uppercase letters, numbers, and symbols.", attempts_per_sec, num_years);
+            }
+        }
+        Ok(())
+    }
+}
---
+++ cli/src/main.rs
@@ -0,0 +1,175 @@
+mod brute_force;
+
+use lorenz::*;
+
+use std::env;
+use std::error::Error;
+use std::path::{Path, PathBuf};
+use std::process::exit;
+use rpassword;
+use sodiumoxide;
+use clap::{App, Arg, ArgGroup};
+
+const FILE_EXTENSION: &str = ".lorenz";
+
+fn main() {
+    let msg = match do_it() {
+        Ok((output_filename, mode)) => {
+            let m = match mode {
+                Mode::Encrypt => "encrypted",
+                Mode::Decrypt => "decrypted",
+            };
+            format!("Success! {} has been {}.", output_filename, m)
+        },
+        Err(e) => format!("{}", e),
+    };
+    println!("{}", msg);
+}
+
+fn do_it() -> Result<(String, Mode), Box<dyn Error>> {
+    sodiumoxide::init().map_err(|_| "sodiumoxide init failed")?;
+
+    let matches = App::new("lorenz")
+        .version("v3.0")
+        .author("Theron Spiegl")
+        .about("lorenz is a simple file encryption utility. Passwords must be at least 12 characters, and a variety of letters, numbers, and symbols is strongly recommended. Written in Rust using sodiumoxide/libsodium's stream encryption. Copyright © 2020 Theron Spiegl. All rights reserved.")
+        .arg(Arg::with_name("encrypt")
+            .short("e")
+            .long("encrypt")
+            .value_name("FILE_TO_ENCRYPT")
+            .help("Specifies the file to encrypt.")
+            .takes_value(true))
+        .arg(Arg::with_name("decrypt")
+            .short("d")
+            .long("decrypt")
+            .value_name("FILE_TO_DECRYPT")
+            .help("Specifies the file to decrypt.")
+            .takes_value(true))
+        .group(ArgGroup::with_name("mode")
+            .args(&["encrypt", "decrypt"])
+            .required(true))
+        .arg(Arg::with_name("output")
+            .short("o")
+            .long("output")
+            .value_name("PATH_TO_OUTPUT_FILE")
+            .help("Specifies a path or name for the output file. If the path to an existing directory is given, the input filename will be kept with the .lorenz extension added if encrypting or removed (if decrypting). Otherwise the file will be placed and named according to this parameter."))
+        .get_matches();
+
+    let mode = if matches.is_present("encrypt") { Mode::Encrypt } else { Mode::Decrypt };
+    let filename = match mode {
+        Mode::Encrypt => matches.value_of("encrypt").ok_or("file to encrypt not given")?,
+        Mode::Decrypt => matches.value_of("decrypt").ok_or("file to decrypt not given")?,
+    };
+
+    let p = Path::new(filename);
+    if !(p.exists() && p.is_file()) {
+        println!("Invalid filename: {}", filename);
+        exit(1);
+    }
+
+    let output_path = generate_output_path(&mode, filename, matches.value_of("output"))?
+        .to_str().ok_or("could not convert output path to string")?.to_string();
+    let password = get_password(&mode);
+
+    let config = Config::new(&mode, password, &filename, &output_path);
+    match main_routine(&config) {
+        Ok(()) => Ok((output_path.to_string(), mode)),
+        Err(e) => Err(e),
+    }
+}
+
+fn get_password(mode: &Mode) -> String {
+    match mode {
+        Mode::Encrypt => {
+            let password = rpassword::prompt_password_stdout("Password (minimum 12 characters, longer is better): ")
+                .expect("could not get password from user");
+            if password.len() < 12 {
+                println!("Error: password must be at least 12 characters. Exiting.");
+                exit(12);
+            }
+            let verified_password = rpassword::prompt_password_stdout("Confirm password: ")
+                .expect("could not get password from user");
+            if password != verified_password {
+                println!("Error: passwords do not match. Exiting.");
+                exit(1);
+            }
+            password
+        },
+        Mode::Decrypt => rpassword::prompt_password_stdout("Password: ").expect("could not get password from user"),
+    }
+}
+
+fn generate_output_path(mode: &Mode, input: &str, output: Option<&str>) -> Result<PathBuf, String> {
+    if output.is_some() { 
+        let p = PathBuf::from(output.unwrap());
+        if p.exists() && p.is_dir() { 
+            generate_default_filename(mode, p, input) 
+        } else if p.exists() && p.is_file() {
+            Err(format!("Error: file {:?} already exists. Must choose new filename or specify directory to generate default filename.", p))
+        } else { 
+            Ok(p)
+        }
+    } else { 
+        let cwd = env::current_dir().map_err(|e| e.to_string())?;
+        generate_default_filename(mode, cwd, input)
+    }
+}
+
+fn generate_default_filename(mode: &Mode, _path: PathBuf, name: &str) -> Result<PathBuf, String> {
+    let mut path = _path;
+    let f = match mode {
+        Mode::Encrypt => {
+            let mut with_ext = name.to_string();
+            with_ext.push_str(FILE_EXTENSION);
+            with_ext
+        },
+        Mode::Decrypt => {
+            if name.ends_with(FILE_EXTENSION) {
+                name[..name.len() - FILE_EXTENSION.len()].to_string()
+            } else {
+                prepend("decrypted_", name).ok_or(format!("could not prepend decrypted_ to filename {}", name))?
+            }
+        },
+    };
+    path.push(f);
+    find_filename(path).ok_or("could not generate filename".to_string())
+}
+
+fn find_filename(_path: PathBuf) -> Option<PathBuf> {
+    let mut i = 1;
+    let mut path = _path;
+    let backup_path = path.clone();
+    while path.exists() {
+        path = backup_path.clone();
+        let stem = match path.file_stem() {
+            Some(s) => s.to_string_lossy().to_string(),
+            None => "".to_string(),
+        };
+        let ext = match path.extension() {
+            Some(s) => s.to_string_lossy().to_string(),
+            None => "".to_string(),
+        };
+        let parent = path.parent()?;
+        let new_file = match ext.as_str() {
+            "" => format!("{} ({})", stem, i),
+            _ => format!("{} ({}).{}", stem, i, ext),
+        };
+        path = [
+            parent,
+            Path::new(&new_file),
+        ].iter().collect();
+        i += 1;
+    }
+    Some(path)
+}
+
+fn prepend(prefix: &str, p: &str) -> Option<String> {
+    let mut path = PathBuf::from(p);
+    let file = path.file_name()?;
+    let parent = path.parent()?;
+    path = [
+        parent,
+        Path::new(&format!("{}{}", prefix, file.to_string_lossy())),
+    ].iter().collect();
+    Some(path.to_string_lossy().to_string())
+}
